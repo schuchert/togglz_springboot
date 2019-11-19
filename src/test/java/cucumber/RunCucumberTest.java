b40c127f2d765960e6affc4b4fab7baa4fb316b3
banana: :dragon <gotdteam+banana-dragon@gmail.com>
2019-11-19 11:47:26 -0500

    Basic running cuke with no steps defined

---
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package cucumber;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        features = "classpath:features",
+        plugin = {"pretty", "html:target/cucumber-reports",
+                "json:target/cucumber.json"
+        }
+
+)
+public class RunCucumberTest {
+}
---
+++ src/test/resources/features/Cukes.feature
@@ -0,0 +1,8 @@
+Feature: The thing
+
+Scenario: One of those things
+
+Given Adeel wants to start running cucumber based tests
+When we add the dependencies
+And set up the necessary files
+Then all will be good and swell
